ab438e423b1a7657a2bb9eb013ece84b88cc7213
Tanisha-0874 <tanishachekkala@gmail.com>
2025-04-03 12:48:25 +0530

    modified java file without code

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -7,8 +7,17 @@ package solutions; // Ensure this matches your package structure
 // }
 import java.util.*;
 
-class Solution {
+public class Solution {
   public List<Integer> findDisappearedNumbers(int[] nums) {
 
+    // Mark numbers present in the array
+
+  }
+
+  // Main method for testings//
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    int[] nums = { 4, 3, 2, 7, 8, 2, 3, 1 };
+    System.out.println(sol.findDisappearedNumbers(nums)); // Output: [5, 6]
   }
 }
