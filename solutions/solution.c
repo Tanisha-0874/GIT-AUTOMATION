4f19e3f16441e344211f51ad709531be5ede44bd
Tanisha-0874 <tanishachekkala@gmail.com>
2025-04-03 10:38:40 +0530

    updated c file with code

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
 
-  //hello
+  for (int i = 0; i < numsSize; i++) {
+    int index = abs(nums[i]) - 1; // Convert value to index
+    if (nums[index] > 0) {
+        nums[index] = -nums[index]; // Mark as visited
+    }
+}
+
+// Step 2: Collect missing numbers
+int* result = (int*)malloc(numsSize * sizeof(int)); // Allocate maximum possible size
+*returnSize = 0;
+
+for (int i = 0; i < numsSize; i++) {
+    if (nums[i] > 0) { // If the number at index i is still positive, (i+1) is missing
+        result[(*returnSize)++] = i + 1;
+    }
+}
+
+return result;
     
 }
