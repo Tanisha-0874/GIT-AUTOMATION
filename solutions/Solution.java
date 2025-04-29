ecb7f0f3a640f18b2f215a1224d3faad67cde800
Tanisha-0874 <tanishachekkala@gmail.com>
2025-04-29 21:14:58 +0530

    Update Solution.java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -4,6 +4,19 @@ import java.util.*;
 
 public class Solution {
   public List<Integer> findDisappearedNumbers(int[] nums) {
+int n = nums.length;
+        boolean[] present = new boolean[n + 1];
+        
+        for (int num : nums) {
+            present[num] = true;
+        }
 
+        List<Integer> missingNumbers = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (!present[i]) {
+                missingNumbers.add(i);
+            }
+        }
+        return missingNumbers;
   }
 }
