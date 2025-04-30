7b22591051d0e95a29bc04025d2c2dcabc7f94e8
Tanisha-0874 <tanishachekkala@gmail.com>
2025-04-30 12:28:59 +0530

    Update solution.c

--- solutions/solution.c
+++ solutions/solution.c
@@ -1,20 +1,3 @@
 int* findDisappearedNumbers(int* nums, int numsSize, int* returnSize) {
-  int temp;
-
-    for (int index = 0; index < numsSize; ++index) {
-        temp = abs(nums[index]) - 1;
-        if (temp >= 0 && temp < numsSize)
-            nums[temp] = -abs(nums[temp]);
-    }
-
-    int* result = (int*)malloc(numsSize * sizeof(int));
-    int insert_index = 0;
-
-    for (int index = 0; index < numsSize; ++index) {
-        if (nums[index] > 0)
-            result[insert_index++] = index + 1;
-    }
-
-    *returnSize = insert_index;
-    return result;
+  
 }
