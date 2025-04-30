c18e583db225a311af693072e2ce54fe12b996ad
Tanisha-0874 <tanishachekkala@gmail.com>
2025-04-30 10:30:25 +0530

    Update solution.cpp

--- solutions/solution.cpp
+++ solutions/solution.cpp
@@ -5,6 +5,18 @@ using namespace std;
 class Solution {
   public:
       vector<int> findDisappearedNumbers(vector<int>& nums) { 
+        int n = nums.size();
+    vector<bool> present(n + 1, false);
+    for (int i=0;i<n;i++) {
+        present[nums[i]] = true;
+    }
+    vector<int> missingNumbers;
+    for (int i = 1; i <= n; i++) {
+        if (!present[i]) {
+            missingNumbers.push_back(i);
+        }
+    }
+    return missingNumbers;
             
       }
 };
