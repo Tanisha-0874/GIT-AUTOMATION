b63c5b31c6f42b1bd9a986362791f00d79d32ee2
Tanisha-0874 <tanishachekkala@gmail.com>
2025-04-30 12:27:56 +0530

    Update solution.py

--- solutions/solution.py
+++ solutions/solution.py
@@ -1,12 +1,6 @@
 class Solution:
     def findDisappearedNumbers(self, nums):
-        num_set = set(nums)
-        n = len(nums)
-        result = []
-        
-        # Check for missing numbers in range [1, n]
-        for i in range(1, n + 1):
-            if i not in num_set:
-                result.append(i)
-        
-        return result
+       """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
